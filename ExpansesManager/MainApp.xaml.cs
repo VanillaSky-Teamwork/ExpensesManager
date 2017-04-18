b12d6d4e9c02a6241413a30fa496bc9aad150b4d
Borkata <borispenev89@gmail.com>
2017-04-18 13:25:12 +0300

    Initail MainApp window logic (still needs more work)

--- ExpansesManager/MainApp.xaml.cs
+++ ExpansesManager/MainApp.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using AutoMapper;
 using Data;
 using ExpansesManager.Core;
+using ExpansesManager.ViewModels;
 using Models.Models;
 
 namespace ExpansesManager
@@ -15,15 +20,47 @@ namespace ExpansesManager
 		public MainApp()
 		{
 			InitializeComponent();
-
-			User user = AuthenticationManager.GetCurrentUser();
+            var vm = new MainAppViewModel();
+			User currentUser = AuthenticationManager.GetCurrentUser();
 			using (var contex = new ExpansesManagerContext())
 			{
-				textBox.Text = "Welcome " +" "+  user.Username;
+				textBox.Text = "Welcome " +" "+ currentUser.Username;
+                var user = contex.Users.Find(currentUser.Id);
+                vm.Groups = new ObservableCollection<GroupViewModel>(Mapper.Instance.Map<IEnumerable<Group>, ObservableCollection<GroupViewModel>>(user.Groups));
+			    this.GroupsGrid.ItemsSource = vm.Groups;
 			}
-
+		    this.GroupsGrid.RowEditEnding += OnRowEditEnding;
 		}
-		protected void Page_Load(object sender, EventArgs e)
+
+	    private void OnRowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+	    {
+	        var s = sender as DataGrid;
+	        var row = e.Row;
+	        var group = e.Row.DataContext as GroupViewModel;
+
+	        if (e.EditAction == DataGridEditAction.Commit)
+	        {
+                
+                using (var db = new ExpansesManagerContext())
+                {
+                    User currentUser = AuthenticationManager.GetCurrentUser();
+                    var user = db.Users.Find(currentUser.Id);
+                    bool doesGroupAlreadyExist = user.Groups.Any(g => g.Name.Equals(group.Name));
+                    if (doesGroupAlreadyExist)
+                    {
+                        
+                    }
+                    else
+                    {
+                        user.Groups.Add(Mapper.Instance.Map<GroupViewModel, Group>(group));
+                        db.SaveChanges();
+                    }
+                }   
+	        }
+	    }
+        
+
+	    protected void Page_Load(object sender, EventArgs e)
 		{
 
 
