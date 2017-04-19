e5b53085ad2d2a0d2e2e50579ff3d0a7e9e410b2
BalthazzarCorr <peev.petar@gmail.com>
2017-04-20 01:54:50 +0300

    Last desperate attempts to make an import work at 2:00  am in the morning but to no avail so Bobka master if you please :)

--- ExpansesManager/ExportGroupToJson.xaml.cs
+++ ExpansesManager/ExportGroupToJson.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Data;
 using ExpansesManager.Core;
 using Models.Models;
@@ -94,9 +83,9 @@ namespace ExpansesManager
 
 		private void Cancel_Click(object sender, RoutedEventArgs e)
 		{
-			MainApp mApp = new MainApp();
+		
 			this.Close();
-			mApp.ShowDialog();
+			
 		}
 	}
 }
